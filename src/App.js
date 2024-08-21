4773171224b9921cb170130d31f779bf4213aa57
omiete01 <estheromiete01@gmail.com>
2024-08-21 18:32:39 +0100

    React App
    A simple react app using javascript, creating a title, a text box and a button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [input, setInput] = useState('');
+
+  const handleImageAnalysis = () => {
+    // Logic for image analysis using the input URL
+    console.log("Analyzing image:", input);
+  };
+
+  const handleImageGeneration = () => {
+    // Logic for image generation using the prompt
+    console.log("Generating image with prompt:", input);
+  };
+
+  const handleInput = (e) => {
+    setInput(e.target.value)
+  }
+
+   return(
+        <div>
+            <h1>Computer Vision</h1>
+            <div>
+                <h3>Insert URL or type prompt:</h3>
+                <form>
+                    <input type="url" value={input} onChange={handleInput} placeholder="Enter url to analyze or textual prompt to generate an image" required />
+                    <div>
+                      <button type="button" onClick={handleImageAnalysis}>Analyze</button>
+                      <button type="button" onClick={handleImageGeneration}>Generate</button>
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
+    )
+
 }
+
+
 
 export default App;
